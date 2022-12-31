a3cc7fd172be19a3fd1fcbf01c033c106c868e1b
ElijahQuinones <118852979+ElijahQuinones@users.noreply.github.com>
2022-12-31 16:36:00 -0500

    Update punch_in.py
    edited script to need no user input

--- punch_in.py
+++ punch_in.py
@@ -1,13 +1,17 @@
 import win32com.client as client
-from datetime import date
+from datetime import datetime
 
-today = date.today()
+today = datetime.now()
 date = today.strftime('%m-%d-%Y')
-shift_start = input("What time did your shift start?")
-Logon =  input ("What time did you log on?")
+
+if today.strftime("%A") == "Saturday":
+    shift_start = "5pm"
+elif today.strftime("%A") == "Sunday":
+    shift_start = "12pm"
+Logon =  today.strftime("%I:%M")
 outlook = client.Dispatch("Outlook.Application")
 message = outlook.CreateItem(0)
 message.Display()
 message.To = "someone@example.com" #replace someone@example.com with where you would like to send the email here
 message.subject = f"Punch in {date} "
-message.Body = f"1. Time your shift starts: {shift_start}  \n2.Time you logged in: {Logon} "
+message.Body = f"1. Time your shift starts: {shift_start}  \n2.Time you logged in: {Logon}"
